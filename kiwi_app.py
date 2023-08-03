b00f7e7e3e1414916bb05724ae8fd9afaedf8009
Yeyu <wenbohuang0307@gmail.com>
2023-08-03 03:25:45 -0700

    Initial commit with two files

---
+++ kiwi_app.py
@@ -0,0 +1,74 @@
+import streamlit as st
+from kiwi_connection import KiwiConnection
+import datetime
+from dateutil.parser import isoparse
+def format_datetime(datetime_str):
+    date_part, time_part = datetime_str.split("T")
+    time_part = time_part[:-5]  # Removing the ".000Z" from the time
+    return f"{date_part} {time_part}"
+
+st.set_page_config(layout="wide")
+st.title("Flight Search")
+st.write("This is an online flight search system that implements Streamlit connection feature to access Kiwi API.")
+conn = st.experimental_connection("kiwiapi", type=KiwiConnection, apikey='iQnT_tFMJE1hTe3nxAO2bDCk6Gb0tXGH')
+
+
+flights = []
+col1, col2 = st.columns([0.5, 0.5], gap='medium')
+with col1:
+    with st.form("my_form"):
+        fly_from = st.text_input("From airport", "JFK")
+        fly_to = st.text_input("To airport", "PRG")
+
+        date_from = st.date_input("Departure date", value=datetime.date(2023, 9, 3))
+        date_to = st.date_input("Return date", value=datetime.date(2023, 9, 9))
+        date_from_str = date_from.strftime("%d/%m/%Y") 
+        date_to_str = date_to.strftime("%d/%m/%Y")
+        adults = st.number_input("Adults", min_value=1) 
+        children = st.number_input("Children", min_value=0)
+        infants = st.number_input("Infants", min_value=0)
+
+        sort = st.selectbox("Sort by", ["price", "duration"])
+        limit = st.number_input("Limit", min_value=10)
+
+        submitted = st.form_submit_button("Submit")
+
+        if submitted:
+            # Call API with input parameters
+            results = conn.query(
+                fly_from=fly_from, 
+                fly_to=fly_to,
+                date_from=date_from_str,
+                date_to=date_to_str,
+                adults=adults,
+                children=children,
+                infants=infants,
+                sort=sort,
+                limit=limit
+            )
+            
+            if results:
+                flights = results['data']
+
+with col2:
+    for flight in flights:
+        
+        overall_departure_time = min(route["local_departure"] for route in flight["route"])
+        overall_arrival_time = max(route["local_arrival"] for route in flight["route"])
+        #total_duration = sum(route["total_duration"] for route in flight["route"].values())
+        city_from = flight["cityFrom"]
+        city_to = flight["cityTo"]
+        fly_from = flight["flyFrom"]
+        fly_to = flight["flyTo"]
+        connection_number = len(flight["route"]) - 1
+        price = flight["price"]
+        
+        overall_departure_time_fm = format_datetime(str(overall_departure_time))
+        overall_arrival_time_fm = format_datetime(str(overall_arrival_time))
+
+        utc_departure_time = flight["route"][0]["utc_departure"]
+        utc_arrival_time = flight["route"][connection_number]["utc_arrival"]
+        total_hours = (isoparse(utc_arrival_time) - isoparse(utc_departure_time)).total_seconds() / 3600
+
+        with st.expander(f'{overall_departure_time_fm} - {overall_arrival_time_fm}, {city_from}/{fly_from} - {city_to}/{fly_to}, total_hours: {total_hours:.1f}, #Connects:{connection_number}, Price:{price}USD'):
+            st.json(flight)
---
+++ kiwi_connection.py
@@ -0,0 +1,48 @@
+from streamlit.connections import ExperimentalBaseConnection
+from streamlit.runtime.caching import cache_data
+
+import requests
+
+class KiwiConnection(ExperimentalBaseConnection):
+    """Basic st.experimental_connection implementation for DuckDB"""
+
+    def _connect(self, **kwargs) -> dict:
+        if 'apikey' in kwargs:
+            key = kwargs.pop('apikey')
+        else:
+            key = self._secrets['apikey']
+        self.headers = {"apikey": key}
+        return self.headers
+
+
+    def query(self, ttl=3600, **kwargs) -> dict:
+        @cache_data(ttl=ttl)
+        def _query(headers, **kwargs) -> dict:
+            BASE_URL = 'https://api.tequila.kiwi.com/v2/search'
+            params = {
+                "fly_from": kwargs.get('fly_from'),
+                "fly_to": kwargs.get('fly_to'),
+                "date_from": kwargs.get('date_from'),
+                "date_to": kwargs.get('date_to'),
+                "adults": kwargs.get('adults'),
+                "children": kwargs.get('children'),
+                "infants": kwargs.get('infants'),
+                "sort": kwargs.get('sort'),
+                "limit": kwargs.get('limit'),
+                "curr": kwargs.get('curr', 'USD')
+            }
+            if kwargs.get('return_from'):
+                params['return_from'] = kwargs.get('return_from')
+            if kwargs.get('return_to'):
+                params['return_to'] = kwargs.get('return_to')
+            if kwargs.get('max_fly_duration'):
+                params['max_fly_duration'] = kwargs.get('max_fly_duration')
+            if kwargs.get('max_sector_stopovers'):
+                params['max_sector_stopovers'] = kwargs.get('max_sector_stopovers')
+
+            response = requests.get(BASE_URL, params=params, headers=headers)
+            response = response.json()
+            
+            return response
+        
+        return _query(self.headers, **kwargs)
